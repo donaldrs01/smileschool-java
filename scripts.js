4f90e306b6fd567e8f01e98dc5e91f844057e744
Ryan Donaldson <128050134+donaldrs01@users.noreply.github.com>
2024-08-01 10:19:40 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -277,7 +277,7 @@ function fetchAndDisplayCourses() {
                 const card = $("<div>").addClass("col-12 col-md-4 col-lg-3 mb-4").append(
                     $("<img>").addClass("card-img-top img-fluid mx-auto").attr("src", course.thumb_url),
                     $("<div>").addClass("card-img-overlay d-flex text-center").append(
-                        $("<img>").addClass("play-overlay position-absolute top-50 start-50 translate-middle").attr("src", "/images/play.png").attr("width", "55px")
+                        $("<img>").addClass("play-overlay position-absolute top-50 start-50 translate-middle").attr("src", "images/play.png").attr("width", "55px")
                     ),
                     $("<div>").addClass("card-main").append(
                         $("<h5>").addClass("card-title").text(course.title),
